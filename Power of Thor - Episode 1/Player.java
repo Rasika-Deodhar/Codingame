e80de658de3b2d1a91eb0111b199a78f243fedef
Rasika Deodhar <rasikadeodhar95@gmail.com>
2022-02-16 23:28:55 +0000

    solved Power of Thor code

--- Power of Thor - Episode 1/Player.java
+++ Power of Thor - Episode 1/Player.java
@@ -6,7 +6,8 @@ import java.math.*;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  * ---
- * Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
+ * Hint: You can use the debug stream to print initialTX and initialTY, if Thor
+ * seems not follow your orders.
  **/
 class Player {
 
@@ -24,44 +25,55 @@ class Player {
             // Write an action using System.out.println()
             // To debug: System.err.println("Debug messages...");
 
-            if(lightX==initialTx){
-                //North
-                if(lightY < initialTy){
+            if (lightX == initialTx) {
+                // North
+                if (lightY < initialTy && initialTy < 18) {
                     System.out.println("N");
+                    --initialTy;
                 }
-                //South
-                 if(lightY > initialTy){
+                // South
+                if (lightY > initialTy && initialTy < 18) {
                     System.out.println("S");
+                    ++initialTy;
                 }
-            }  else if(lightY==initialTy){
-                //West
-                if(lightX < initialTx){
+            } else if (lightY == initialTy) {
+                // West
+                if (lightX < initialTx && initialTx < 40) {
                     System.out.println("W");
+                    --initialTx;
                 }
-                //East
-                 if(lightX > initialTx){
+                // East
+                if (lightX > initialTx && initialTx < 40) {
                     System.out.println("E");
+                    ++initialTx;
                 }
             } else if (lightX < initialTx && lightY < initialTy
-            && initialTx < 40 && initialTy < 18) {
-                //North West
+                    && initialTx < 40 && initialTy < 18) {
+                // North West
                 System.out.println("NW");
+                --initialTy;
+                --initialTx;
             } else if (lightX < initialTx && lightY > initialTy
-            && initialTx < 40 && initialTy < 18) {
-                //South West
+                    && initialTx < 40 && initialTy < 18) {
+                // South West
                 System.out.println("SW");
+                ++initialTy;
+                --initialTx;
             } else if (lightX > initialTx && lightY < initialTy
-            && initialTx < 40 && initialTy < 18) {
-                //North East
+                    && initialTx < 40 && initialTy < 18) {
+                // North East
                 System.out.println("NE");
+                --initialTy;
+                ++initialTx;
             } else if (lightX > initialTx && lightY > initialTy
-            && initialTx < 40 && initialTy < 18) {
-                //South East
+                    && initialTx < 40 && initialTy < 18) {
+                // South East
                 System.out.println("SE");
+                ++initialTy;
+                ++initialTx;
             } else {
                 break;
             }
-
 
             // A single line providing the move to be made: N NE E SE S SW W or NW
             // System.out.println("SE");
